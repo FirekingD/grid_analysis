3e8ee373fcb00837be07b8b67aab486e7646e4c5
FirekingD <maghraouidhia@gmail.com>
2022-01-03 14:17:58 +0100

    updated annual data aggregation code

--- main.R
+++ main.R
@@ -20,21 +20,22 @@ prod_elec<- read.csv("./data/production-electrique-par-filiere-a-la-maille-regio
 iris_paris<-st_read(dsn="data",layer="iris_paris")
 #plot(iris_paris)
 iris_paris_joined_conso<-merge(iris_paris,conso_elec,by.x="CODE_IRIS",by.y="Code.IRIS",type="full")
+iden<-c("Consommation.annuelle.moyenne.par.logement.de.l.adresse..MWh.","Nombre.de.logements")
 #plot(iris_paris_joined_conso$geometry)
 c2018=filter(iris_paris_joined_conso,Année=="2018")
 c2018$Consommation.annuelle.moyenne.par.logement.de.l.adresse..MWh.= as.numeric(sub(",",".",c2018$Consommation.annuelle.moyenne.par.logement.de.l.adresse..MWh.,fixed=TRUE))
-c2018_agr=aggregate(c2018$Consommation.annuelle.moyenne.par.logement.de.l.adresse..MWh., list(c2018$CODE_IRIS), sum)
-names(c2018_agr)[1] <- "code_iris" ;names(c2018_agr)[2] <- "conso_annuelle_totale_iris" 
+c2018_agr=aggregate(c2018[iden], list(c2018$CODE_IRIS), sum)
+names(c2018_agr)[1] <- "code_iris" ;names(c2018_agr)[2] <- "conso_annuelle_totale_iris" ; names(c2018_agr)[3]<-"nb_logements_totale_iris"
 
 c2019=filter(iris_paris_joined_conso,Année=="2019")
 c2019$Consommation.annuelle.moyenne.par.logement.de.l.adresse..MWh.= as.numeric(sub(",",".",c2019$Consommation.annuelle.moyenne.par.logement.de.l.adresse..MWh.,fixed=TRUE))
-c2019_agr=aggregate(c2019$Consommation.annuelle.moyenne.par.logement.de.l.adresse..MWh., list(c2019$CODE_IRIS), sum)
-names(c2019_agr)[1] <- "code_iris" ;names(c2019_agr)[2] <- "conso_annuelle_totale_iris" 
+c2019_agr=aggregate(c2019[iden], list(c2019$CODE_IRIS), sum)
+names(c2019_agr)[1] <- "code_iris" ;names(c2019_agr)[2] <- "conso_annuelle_totale_iris" ; names(c2019_agr)[3]<-"nb_logements_totale_iris"
 
 c2020=filter(iris_paris_joined_conso,Année=="2020")
 c2020$Consommation.annuelle.moyenne.par.logement.de.l.adresse..MWh.= as.numeric(sub(",",".",c2020$Consommation.annuelle.moyenne.par.logement.de.l.adresse..MWh.,fixed=TRUE))
-c2020_agr=aggregate(c2020$Consommation.annuelle.moyenne.par.logement.de.l.adresse..MWh., list(c2020$CODE_IRIS), sum)
-names(c2020_agr)[1] <- "code_iris" ;names(c2020_agr)[2] <- "conso_annuelle_totale_iris" 
+c2020_agr=aggregate(c2020[iden], list(c2020$CODE_IRIS), sum)
+names(c2020_agr)[1] <- "code_iris" ;names(c2020_agr)[2] <- "conso_annuelle_totale_iris" ; names(c2020_agr)[3]<-"nb_logements_totale_iris"
 
 
 #ggplot()+geom_polygon(data=iris_paris_joined_conso2018,aes(long,lat,fill=Consommation.annuelle.moyenne.par.logement.de.l.adresse..MWh.))
